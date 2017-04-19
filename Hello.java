c9bd2d2c638b652ac873e287004eae2bca77547c
ankitgpt004 <ankitgpt004@users.noreply.github.com>
2017-04-19 11:16:41 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
-class Hello{
-public static void main(String args[]){
-System.out.println("Helloohjhh");
-}
+//program for prime number
+class PrimeNumber{
+static int n=125;
+static int m=n/2;
+  public static void main(String args[]){
+  for(int i=2;i<=m;i++){
+  if(n%i==0){
+  System.out.println("number is not prime");
+    break;
+   }
+    else
+      System.out.println("number is prime");
+  }
+ }
 }
